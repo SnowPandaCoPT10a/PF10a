1a7e07be0c6f602d4fd4d15ebe5cbef80b3f6035
FacuEtcheverryL <105181693+FacuEtcheverryL@users.noreply.github.com>
2023-03-15 01:36:25 -0300

    modulado
    un poco mas de orden

---
+++ api/src/controllers/accessoriesControlles.js
@@ -0,0 +1,3 @@
+const { Accessories} = require("../db");
+const { Op } = require("sequelize");
+
--- api/src/db.js
+++ api/src/db.js
@@ -22,7 +22,11 @@ fs.readdirSync(path.join(__dirname, '/models'))
   });
 
 // Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach(model => model(sequelize));
+modelDefiners.forEach(model => {
+  if (typeof model === 'function') {
+    model(sequelize);
+  }
+});
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
@@ -30,7 +34,18 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Products } = sequelize.models;
+const { 
+  Accessories,
+  Bills,
+  Boards,
+  Boots,
+  Favorites,
+  Jackets,
+  Pants,
+  Reviews,
+  TShirts,
+  Users,
+ } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
@@ -39,3 +54,4 @@ module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
 };
+ 
