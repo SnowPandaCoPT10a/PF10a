a9bbda9c1db4ecf129c2c4e506f916e2e3843d6d
FacuGomez2022 <fgfacundogomez@hotmail.com>
2023-03-15 11:45:00 -0300

    End of .json from DATA and models of products

---
+++ api/src/models/TShirts.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("tshirts", {
+    tshirtsID: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    sizes: {
+      type: DataTypes.JSON,
+    },
+    img: {
+      type: DataTypes.STRING,
+    },
+    material: {
+      type: DataTypes.STRING,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    activity: {
+      type: DataTypes.STRING,
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+  });
+};
