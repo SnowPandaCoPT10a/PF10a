f51437ef7c3f6754eaa52fc9d0c10d6543159648
FacuEtcheverryL <105181693+FacuEtcheverryL@users.noreply.github.com>
2023-03-21 13:00:19 -0300

    AccessoriesControllers

--- api/src/models/Boards.js
+++ api/src/models/Boards.js
@@ -39,5 +39,9 @@ module.exports = (sequelize) => {
       type: DataTypes.BOOLEAN,
       defaultValue: false,
     },
+    status: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    },
   });
 };
--- api/src/routes/accesoriesRouter.js
+++ api/src/routes/accesoriesRouter.js
@@ -1,5 +1,5 @@
 const {Router} = require('express');
-const {getAllAccessories,getAccessoriesById,postNewAccessories} = require('../controllers/accessoriesControlles');
+const {getAllAccessories,getAccessoriesById,postNewAccessories,deleteAccessories,modifyAccessories,disableAccessorie} = require('../controllers/accessoriesControlles');
 
 const router = Router();
 
@@ -7,5 +7,9 @@ router.get('/', getAllAccessories);
 router.get('/:id', getAccessoriesById);
 
 router.post('/create', postNewAccessories);
+router.put('/disable/:accessoriesID',disableAccessorie)
+router.put('/modify/:accessoriesID',modifyAccessories)
+
+router.delete('/delete/:accessoriesID', deleteAccessories)
 
 module.exports = router;
