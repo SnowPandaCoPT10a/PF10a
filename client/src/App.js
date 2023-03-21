3634371abee1128af870e5446086cb733cfaf32e
Pauli <94504453+Sil-Pauli@users.noreply.github.com>
2023-03-21 20:24:14 -0300

    App.js

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Route , Routes } from 'react-router-dom';
+import Home from './components/Home';
+
+
+function App() {
+  return (
+    
+    <div className='App'>
+      <Routes>
+        <Route exact path='/home' element={<Home/>} />
+        </Routes>  
+        </div>
+    
+  );
+}
+
+export default App;
