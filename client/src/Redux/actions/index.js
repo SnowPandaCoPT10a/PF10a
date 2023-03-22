46bd1dd888524e783627b385b92b1aadcd3fe5c8
LucianoRp1 <105827151+LucianoRp1@users.noreply.github.com>
2023-03-22 20:16:51 -0300

    AHORA SI GATO

--- client/src/Redux/actions/index.js
+++ client/src/Redux/actions/index.js
@@ -1,11 +1,11 @@
 import {GET_ALL_ACCESSORIES, GET_ALL_TSHIRTS} from '../actions-types/index.js'
-
+const { REACT_APP_GET_ALL_ACCESSORIES, REACT_APP_GET_ALL_TSHIRTS } = process.env;
 
 
 export function getAccessories() {
   return async function (dispatch) {
     try {
-      const response = await axios.get('http://localhost:3001/accessories');
+      const response = await axios.get(REACT_APP_GET_ALL_ACCESSORIES);
       if (response.status === 202) {
         return dispatch({
           type: GET_ALL_ACCESSORIES,
@@ -24,7 +24,7 @@ export function getAccessories() {
 export function getTshirts() {
   return async function (dispatch) {
     try {
-      const response = await axios.get('http://localhost:3001/tshirts');
+      const response = await axios.get(REACT_APP_GET_ALL_TSHIRTS);
       if (response.status === 202) {
         return dispatch({
           type: GET_ALL_TSHIRTS,
