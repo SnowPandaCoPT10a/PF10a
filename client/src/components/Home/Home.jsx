1b8f85873b7bcbea4b9e4bf8494691907c5c16fe
cuevasgaston <gaston322@hotmail.com>
2023-03-21 23:24:03 -0300

    form

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -4,10 +4,18 @@ import React from 'react'
 // import { Link } from 'react-router-dom';
 // import Header from './components/';
  import  './Home.css';
+ import Form from '../Form/Form';
 
 const Home = () => {
   return (
+    <div>
     <div className='component mt-5'><h1>SnowPandaCo</h1></div>
+    <div><h4>Life is like a snowboard: find your balance, keep up the speed, and enjoy the ride. If you fall, get up again and keep going stronger than ever.</h4></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <Form/>
+    </div>
   )
 }
 
