0a8cbdea3e3ce40821e4fbc82e746c817a438866
Agustinn-1002 <71684855+Agustinn-1002@users.noreply.github.com>
2023-03-24 18:53:28 -0300

    tienda 2.0
    aguante mariela

--- client/src/components/tiendaCategorias/categorias/Categorias.jsx
+++ client/src/components/tiendaCategorias/categorias/Categorias.jsx
@@ -9,7 +9,7 @@ const Categorias = () => {
       <h2 className='text-center mt-5'>CATEGORIAS</h2>
       <div className=" row text-center d-flex justify-content-center lign-items-center contianer">
       {data.map(e =>
-          <Link to={`/${e.name}`} className="col-2">
+          <Link to={`/${e.name}`} key={e.name} className="col-2">
             <img className="w-100 rounded-circle" src={e.img} alt="" />
             <h2 className="fw-normal">{e.name}</h2>
           </Link>
