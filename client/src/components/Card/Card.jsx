99b1d640ee83165a6a652f74de78af7bab66adf2
Agustinn-1002 <71684855+Agustinn-1002@users.noreply.github.com>
2023-03-27 20:56:47 -0300

    Card / detaild
    css medianamente arreglado

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -12,39 +12,39 @@ export default function Card() {
     const dispatch = useDispatch()
     const productInfoId = useSelector((state) => state.productsID);
 
-    useEffect(() =>{
-    dispatch(getAllProductsId(id))
-  },[id ,dispatch])
+    useEffect(() => {
+        dispatch(getAllProductsId(id))
+    }, [id, dispatch])
 
 
 
-// console.log(productInfoId, 'ididid')
-// console.log(productInfoId.sizes?.map(el => el.size));
-    return ( 
-            <div>
-             <div className="containerId">
-        <div className="imgBx">
-            <img className='imgId'  src={productInfoId.img} alt="Img not found" />
-        </div>
-        <div className="details">
-            <div className="contentId">
-                <h2 className='pName'>{productInfoId.name}<br/>
-                    <span className='spanId'>{productInfoId.brand}</span>
-                </h2>
-                <p className='pIds'>
-                   {productInfoId.description}
-                </p>
-                {/*<h4  className="imgBx2" data-brand={productInfoId.model}></h4>*/}
-                <p className='pIds'>
-                 { productInfoId.numbersizes?.map(el => el.size + '-')   ||  productInfoId.sizes?.map(el => el.size + '-' )  }
-                </p>
-               
-                <h3 className='h3Name'>${productInfoId.price}</h3>
-                <button>Buy Now</button>
+    // console.log(productInfoId, 'ididid')
+    // console.log(productInfoId.sizes?.map(el => el.size));
+    return (
+        <div className="cardComponent">
+            <div className="containerId">
+                <div className="imgBx">
+                    <img className='imgId' src={productInfoId.img} alt="Img not found" />
+                </div>
+                <div className="details">
+                    <div className="contentId">
+                        <h2 className='pName'>{productInfoId.name}<br />
+                            <span className='spanId'>{productInfoId.brand}</span>
+                        </h2>
+                        <p className='pIds'>
+                            {productInfoId.description}
+                        </p>
+                        {/*<h4  className="imgBx2" data-brand={productInfoId.model}></h4>*/}
+                        <p className='pIds'>
+                            {productInfoId.numbersizes?.map(el => el.size + '-') || productInfoId.sizes?.map(el => el.size + '-')}
+                        </p>
+
+                        <h3 className='h3Name'>${productInfoId.price}</h3>
+                        <button>Buy Now</button>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-            </div>
 
     );
 }
