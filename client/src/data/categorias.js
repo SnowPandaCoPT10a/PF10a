e1eb0bbd214c0c6983ab5089ba035dbe298d4fe6
Agustinn-1002 <agupasten@gmail.com>
2023-03-30 11:35:21 -0300

    Rutas Front
    Modularizado

--- client/src/data/categorias.js
+++ client/src/data/categorias.js
@@ -1,26 +1,26 @@
 export const data = [
     {
-         name: 'Accesories',
+         name: 'accessories',
          img: 'https://res.cloudinary.com/dberwyxyq/image/upload/v1679220197/SnowPandaCO/Accessories/MaxProtect%20helmet.png'
     },
     {
-        name: 'Board',
+        name: 'board',
         img: 'https://res.cloudinary.com/dberwyxyq/image/upload/v1679322574/SnowPandaCO/Boards/Lib%20Tech%20TRS.png '
     },
     {
-        name: 'Pants',
+        name: 'pants',
         img: 'https://res.cloudinary.com/dberwyxyq/image/upload/v1679425818/SnowPandaCO/Pants/SlimCo.png'
     },
     {
-        name: 'Boots',
+        name: 'boots',
         img: 'https://res.cloudinary.com/dberwyxyq/image/upload/v1679331948/SnowPandaCO/Boots/CrossCountry.png'
     },
     {
-        name: 'Tshirts',
+        name: 't-shirts',
         img: 'https://res.cloudinary.com/dberwyxyq/image/upload/v1679421809/SnowPandaCO/TShirts/Grit.png'
     },
     {
-        name: 'Jackets',
+        name: 'jackets',
         img: 'https://res.cloudinary.com/dberwyxyq/image/upload/v1679427907/SnowPandaCO/Jackets/Urbana%202.0.png'
     },
 ]
