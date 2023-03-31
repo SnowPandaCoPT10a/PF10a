1873f77e3ef2266a90f95fe48a50eead420eadbd
FacuGomez2022 <fgfacundogomez@hotmail.com>
2023-03-31 04:49:34 -0300

    Removí código viejo del form. Agregué modelo de address al usuario. Agregué información a renderizar por las cards. Agregado de objeto de brands y logos. Removí archivos que ya no se usan

--- client/src/components/tiendaCategorias/Tienda.jsx
+++ client/src/components/tiendaCategorias/Tienda.jsx
@@ -1,34 +1,34 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import {getAllProducts} from '../../Redux/actions/index'
-import Banner from './banner/Banner';
-import Categorias from './categorias/Categorias';
-import Destacados from './destacados/Destacados';
-import Marcas from './marcas/Marcas';
-import './style.css'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllProducts } from "../../Redux/actions/index";
+import Banner from "./banner/Banner";
+import Categorias from "./categorias/Categorias";
+import Destacados from "./destacados/Destacados";
+import Marcas from "./marcas/Marcas";
+import "./style.css";
 
 const tienda = () => {
-  const datos = useSelector(e=>e.allProducts)
-  const dispatch = useDispatch()
+  const datos = useSelector((state) => state.allProducts);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getAllProducts())
-  }, [])
+    dispatch(getAllProducts());
+  }, []);
 
   const scroll = () => {
     window.scroll({
       top: 0,
-      left: 0
-    })
-  }
+      left: 0,
+    });
+  };
   return (
-    <div className='tiendaCont p-0 m-0 text-center'>
-       <Banner/>
-       <Categorias scroll={scroll}/>
-       <Destacados datos={datos}/>
-       <Marcas scroll={scroll} datos={datos}/>
+    <div className="tiendaCont p-0 m-0 text-center">
+      <Banner />
+      <Categorias scroll={scroll} />
+      <Destacados datos={datos} />
+      <Marcas scroll={scroll} datos={datos} />
     </div>
-  )
-}
+  );
+};
 
-export default tienda
+export default tienda;
