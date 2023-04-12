c57dfcd1d725c2dd6b8c17ddbf288785e7c3cbf0
Pauli <94504453+Sil-Pauli@users.noreply.github.com>
2023-04-12 00:47:09 -0300

    mas cambios
    pintamos toda la casa

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import Cards from "./components/Cards/Cards";
 import Card from "./components/Card/Card";
 import Create from "./components/Create/Create";
 import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
-import { useNavigate } from 'react-router-dom';
+//import { useNavigate } from 'react-router-dom';
 import Profile from './components/Profile/Profile.jsx';
 import './App.css'
 import { useState } from 'react';
@@ -56,32 +56,9 @@ const [oneProducts, setOneProducts] = useState([]);
           <Route exact path='/' element={<Home setCountProducts={setCountProducts} setAllProducts={setAllProducts} setPriceTotal={setPriceTotal}/>} />
           <Route exact path='/Home' element={<Home />} />
           <Route exact path="/Members" element={<Members />} />
-          
           <Route exact path='/ShoppingCart' element={<ShoppingCart oneProducts={oneProducts} setOneProducts={setOneProducts} allProducts={allProducts} setAllProducts={setAllProducts} priceTotal={priceTotal} setPriceTotal={setPriceTotal}  countProducts={countProducts}  setCountProducts={setCountProducts} />} />
-
           <Route exact path='/Products/:article' element={<Cards/>}/>
-
-          {/* <Route exact path='/Board' element={<Cards categoria='board' path='/Board' />} />
-          <Route exact path='/Boots' element={<Cards categoria='boots' path='/Boots' />} />
-          <Route exact path='/Jackets' element={<Cards categoria='jackets' path='/Jackets' />} />
-          <Route exact path='/Pants' element={<Cards categoria='pants' path='/Pants' />} />
-          <Route exact path='/t-shirts' element={<Cards categoria='t-shirts' path='/t-shirts' />} />
-          <Route exact path='/Accesories' element={<Cards categoria='accessories' path='/Accesories' />} />
-
-          <Route exact path='/SnowPandaCo' element={<Cards categoria='SnowPandaCo' path='/SnowPandaCo' />} />
-          <Route exact path='/Burton' element={<Cards categoria='Burton' path='/Burton' />} />
-          <Route exact path='/Nitro' element={<Cards categoria='Nitro' path='/Nitro' />} />
-          <Route exact path='/K2' element={<Cards categoria='K2' path='/K2' />} />
-          <Route exact path='/Rossignol' element={<Cards categoria='Rossignol' path='/Rossignol' />} />
-          <Route exact path='/Lib Tech' element={<Cards categoria='Lib Tech' path='/Lib Tech' />} />
-          <Route exact path='/Arbor' element={<Cards categoria='Arbor' path='/Arbor' />} />
-          <Route exact path='/Capita' element={<Cards categoria='Capita' path='/Capita' />} />
-          <Route exact path='/Gnu' element={<Cards categoria='Gnu' path='/Gnu' />} />
-          <Route exact path='/Jones' element={<Cards categoria='Jones' path='/Jones' />} />
-          <Route exact path='/Rome' element={<Cards categoria='Rome' path='/Rome' />} /> */}
-
           <Route path= '/User' element={<Profile />} />
-          
          <Route path='/Products/:categoria/:id/Detail' element={<Card oneProducts={oneProducts} setOneProducts={setOneProducts} allProducts={allProducts} setAllProducts={setAllProducts} priceTotal={priceTotal} setPriceTotal={setPriceTotal}  countProducts={countProducts}  setCountProducts={setCountProducts} />} />
          <Route exact path='/Create' element={<Create />} />
          <Route path='/OrderConfirmation' element={<OrderConfirmation />} /> 
@@ -89,7 +66,6 @@ const [oneProducts, setOneProducts] = useState([]);
          <Route exact path='/Products' element={<Cards/>} />
          <Route exact path='/ManageProducts' element={<Edit/>} />
          <Route exact path='/ManageProfiles' element={<ManageProfiles/>} />
-         
         </Routes>
         <Chatbot />
       </div>
