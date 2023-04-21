9899fbb08c0ed4fba388719971f3a1a40051f3bf
Agustinn-1002 <71684855+Agustinn-1002@users.noreply.github.com>
2023-04-20 23:18:19 -0300

    demo

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -17,7 +17,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllUsers } from "../../Redux/actions/index";
 import { createContext } from "react";
-import { BsFillCircleFill, BsFillMoonFill } from "react-icons/bs";
+import { BsFillCircleFill, BsFillMoonFill, BsThreeDotsVertical } from "react-icons/bs";
 import ReactSwitch from "react-switch";
 
 const Header = ({
@@ -33,30 +33,32 @@ const Header = ({
   const dispatch = useDispatch();
   const datos = useSelector((e) => e.user);
   const { isAuthenticated, isLoading, user } = useAuth0();
-  
+
+  let ancho = window.screen
 
   let perfil = null;
 
   try {
-    perfil = datos.filter((obj) =>obj.email === user.email); // estoy matcheadn que el email de aut0 este en  la base de datos 
-    
+    perfil = datos.filter((obj) => obj.email === user.email); // estoy matcheadn que el email de aut0 este en  la base de datos 
+
   } catch (error) {
     console.log('Esperando datos');
   }
   let isAdmin = null;
-  
-  try {
-    isAdmin =datos.filter((obj)=>obj.email === user.email && obj.privilige === true);
-    
-     }
-    catch (error) {
-      console.log('Esperando datos');
-    }
 
-  
+  try {
+    isAdmin = datos.filter((obj) => obj.email === user.email && obj.privilige === true);
+
+  }
+  catch (error) {
+    console.log('Esperando datos');
+  }
+
+
 
   const [dropActive, setDropActive] = useState(false);
-
+  const [dropNavResponsive, setDropNavResponsive] = useState(false);
+  console.log(dropNavResponsive);
   useEffect(() => {
     dispatch(getAllUsers());
   }, [dispatch]);
@@ -88,170 +90,185 @@ const Header = ({
       <Link to={"/"} onClick={() => scrollTop()}>
         <img src={Logo} alt="" className="imgLogo" />
       </Link>
-
-      {/* {isProductCategoryPage && <SearchBar categories={categories} />} */}
-      <div>
-        {location.pathname !== "/" && <SearchBar categories={categories} />}
-      </div>
-
-      <Link to={"/Shop"} onClick={() => scrollTop()}>
-        <button
-          className={rutaUrl.includes("Shop") ? "btnHome active" : "btnHome"}
-        >
-          Shop
-        </button>
-      </Link>
-
-      {!rutaUrl.includes("Shop") && !rutaUrl.includes("Products") ? (
-        <>
-          <a href="/#AboutUs" className="btnRedirectHome">
-            About Us
-          </a>
-          <a href="/#OurValues" className="btnRedirectHome">
-            Our Values
-          </a>
-          <a href="/#contacto" className="btnRedirectHome">
-            Contact Us
-          </a>
-        </>
-      ) : (
-        <>
-          <a href="/Shop#categoria" className="btnRedirectHome">
-            Categorias
-          </a>
-          <a href="/Shop#marcas" className="btnRedirectHome">
-            Marcas
-          </a>
-        </>
-      )}
-
-      {!isAuthenticated ? (
-        <Login />
-      ) : (
-        <div
-          onMouseOver={() => setDropActive(true)}
-          onMouseOut={() => setDropActive(false)}
-        >
-          <button
-            className={
-              rutaUrl.includes("User")
-                ? "btnUser active border border-white"
-                : "btnUser"
-            }
-          >
-            <FaUserAlt />
-          </button>
-
-          <div className={dropActive ? "drop-active" : "drop-null"}>
-            {dropActive ? (
-              <>
-                <Link to="/User" className="text-decoration-none">
-                  <a>Ver Perfil</a>
-                </Link>
-                <a class="dropdown-item" href="/ProfileBills">
-                  Mis Compras
-                </a>
-                <a class="dropdown-item" href="/ProfileReview">
-                  Mis Reviews
-                </a>
-                <a>
-                  <Logout />
-                </a>
-                {isAdmin?.length ? <div class="">
-                  <button
-                    class="button-AdminManage dropdown-toggle ps-0"
-                    id="bd-versions"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                    data-bs-display="static"
-                  >
-                    <span class="d-none d-lg-inline">Admin Console</span>
-                  </button>
-                  <ul
-                    class="dropdown-menu dropdown-menu-end mt-0 pt-0"
-                    aria-labelledby="bd-versions"
-                  >
-                    <li>
-                      <a
-                        class="dropdown-item text-black"
-                        href="/ManageProfiles"
-                      >
-                        Manage Profiles
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        class="dropdown-item text-black"
-                        href="/ManageProducts"
-                      >
-                        Manage Products
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item text-black" href="/ManageBills">
-                        Manage Bills
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item text-black" href="/ManageReviews">
-                        Manage Reviews
-                      </a>
-                    </li>
-                  </ul>
-                </div>: null}
-              </>
-            ) : null}
-          </div>
+      <div className="ps-5 d-flex flex-row justify-content-around align-items-center w-75">
+        {/* {isProductCategoryPage && <SearchBar categories={categories} />} */}
+        <div>
+          {location.pathname !== "/" && <SearchBar categories={categories} />}
         </div>
-      )}
-      {!isAuthenticated ? null : (
-        <Link to={"/ShoppingCart"}>
-          <button className="btnCarrt">
-            <FaShoppingCart />
-            <span className="notificacionCantidad badge rounded-pill bg-danger ">
-              {countProducts}
-            </span>
+
+
+        <Link to={"/Shop"} onClick={() => scrollTop()}>
+          <button
+            className={rutaUrl.includes("Shop") ? "btnHome active" : "btnHome"}
+          >
+            Shop
           </button>
         </Link>
-      )}
-      <div className="switch">
-        {/* <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label> */}
-        <ReactSwitch
-          onChange={toggleTheme}
-          checked={theme === "light"}
-          offColor="#333"
-          onColor="#ccc"
-          onHandleColor="#000"
-          uncheckedIcon={
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "100%",
-                paddingRight: 2,
-                color: "white",
-              }}
+
+
+
+        {!rutaUrl.includes("Shop") && !rutaUrl.includes("Products") ?
+          <>
+            <a href="/#AboutUs" className="btnRedirectHome">
+              About Us
+            </a>
+            <a href="/#OurValues" className="btnRedirectHome">
+              Our Values
+            </a>
+            <a href="/#contacto" className="btnRedirectHome">
+              Contact Us
+            </a>
+          </>
+          :
+          <>
+            <a href="/Shop#categoria" className="btnRedirectHome">
+              Categorias
+            </a>
+            <a href="/Shop#marcas" className="btnRedirectHome">
+              Marcas
+            </a>
+          </>}
+        
+        {!isAuthenticated ? (
+          <Login />
+        ) : (
+          <div
+            onMouseOver={() => setDropActive(true)}
+            onMouseOut={() => setDropActive(false)}
+          >
+            <button
+              className={
+                rutaUrl.includes("User")
+                  ? "btnUser active border border-white"
+                  : "btnUser"
+              }
             >
-              <BsFillMoonFill />
+              <FaUserAlt />
+            </button>
+
+            <div className={dropActive ? "drop-active" : "drop-null"}>
+              {dropActive ? (
+                <>
+                  <Link to="/User" className="text-decoration-none">
+                    <a>Ver Perfil</a>
+                  </Link>
+                  <a className="dropdown-item" href="/ProfileBills">
+                    Mis Compras
+                  </a>
+                  <a className="dropdown-item" href="/ProfileReview">
+                    Mis Reviews
+                  </a>
+                  <a>
+                    <Logout />
+                  </a>
+                  {isAdmin?.length ? <div className="">
+                    <button
+                      className="button-AdminManage dropdown-toggle ps-0"
+                      id="bd-versions"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                      data-bs-display="static"
+                    >
+                      <span className="d-none d-lg-inline">Admin Console</span>
+                    </button>
+                    <ul
+                      className="dropdown-menu dropdown-menu-end mt-0 pt-0"
+                      aria-labelledby="bd-versions"
+                    >
+                      <li>
+                        <a
+                          className="dropdown-item text-black"
+                          href="/ManageProfiles"
+                        >
+                          Manage Profiles
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          className="dropdown-item text-black"
+                          href="/ManageProducts"
+                        >
+                          Manage Products
+                        </a>
+                      </li>
+                      <li>
+                        <a className="dropdown-item text-black" href="/ManageBills">
+                          Manage Bills
+                        </a>
+                      </li>
+                      <li>
+                        <a className="dropdown-item text-black" href="/ManageReviews">
+                          Manage Reviews
+                        </a>
+                      </li>
+                    </ul>
+                  </div> : null}
+                </>
+              ) : null}
             </div>
-          }
-          checkedIcon={
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "100%",
-                paddingRight: 2,
-                color: "yellow",
-              }}
-            >
-              <BsFillCircleFill />
+          </div>
+        )}
+
+        {!isAuthenticated ? null : (
+          <div className="d-flex justify-content-between align-items-center">
+            <Link to={"/ShoppingCart"}>
+              <button className="btnCarrt">
+                <FaShoppingCart />
+                <span className="notificacionCantidad badge rounded-pill bg-danger ">
+                  {countProducts}
+                </span>
+              </button>
+            </Link>
+            <div className="ms-4 px-4 fs-1 buttonHamburgerNav" onClick={() => setDropNavResponsive(!dropNavResponsive)}>
+              <BsThreeDotsVertical className="text-white" />
             </div>
-          }
-        />
+          </div>
+        )}
+
+
+
+
+
+        <div className="switch">
+          {/* <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label> */}
+          <ReactSwitch
+            onChange={toggleTheme}
+            checked={theme === "light"}
+            offColor="#333"
+            onColor="#ccc"
+            onHandleColor="#000"
+            uncheckedIcon={
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "100%",
+                  paddingRight: 2,
+                  color: "white",
+                }}
+              >
+                <BsFillMoonFill />
+              </div>
+            }
+            checkedIcon={
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "100%",
+                  paddingRight: 2,
+                  color: "yellow",
+                }}
+              >
+                <BsFillCircleFill />
+              </div>
+            }
+          />
+        </div>
       </div>
+
     </div>
   );
 };
